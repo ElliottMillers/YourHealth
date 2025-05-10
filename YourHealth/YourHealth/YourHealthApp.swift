aca13da9ae7cd0097b7fbcfe47f2697cba831d31
ElliottMillers <millersdev.ios@gmail.com>
2025-05-10 20:37:38 +0300

    Section 1: Added project

---
+++ YourHealth/YourHealth/YourHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourHealthApp.swift
+//  YourHealth
+//
+//  Created by Rostyslav Mykhal on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YourHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
